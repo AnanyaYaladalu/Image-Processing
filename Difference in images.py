34438ff0c15e53b65af86c053e11631a727246f1
AnanyaYaladalu <108124837+AnanyaYaladalu@users.noreply.github.com>
2023-01-03 16:25:42 +0530

    Add files via upload

---
+++ Difference in images.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+from PIL import Image, ImageChops
+
+img1 = Image.open("1img.jpg")
+img2 = Image.open("2img.jpg")
+
+diff = ImageChops.difference(img1, img2)
+diff.show()
+
+
+# In[ ]:
+
+
+
+
